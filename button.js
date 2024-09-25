cdd3c4efe5fbd4eef7ec2e0256194145ace1d3b5
sa7ik <mohammedsabikoo7@gmail.com>
2024-09-25 14:59:24 +0530

    updated

---
+++ button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function button() {
+  return (
+    <div>
+        <button>add</button>
+    </div>
+  )
+}
+
+export default button
---
+++ component b.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function component b() {
+  return (
+    <div>component b</div>
+  )
+}
+
+export default component b
